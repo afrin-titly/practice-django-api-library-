78564741f36ec0239080bd6f8f620ed7b1e152a1
lm-afrin <titlee42@gmail.com>
2021-11-18 20:30:47 +0900

    create django book app

---
+++ books/views.py
@@ -0,0 +1,7 @@
+from django.db import models
+from .models import Book
+from django.views.generic import ListView
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
